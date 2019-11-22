2e8b8d29c84ceb7bad2fb1856d643819b97c05c7
talita.santos <talita.santos@horizonci.com.br>
2019-11-22 09:14:19 -0300

    tela de splash

--- app/src/main/java/com/example/nivelamento_tres/DisciplinaActivity.java
+++ app/src/main/java/com/example/nivelamento_tres/DisciplinaActivity.java
@@ -8,15 +8,38 @@ import android.widget.Button;
 
 public class DisciplinaActivity extends AppCompatActivity {
 
+    protected EditText editTextDisciplin;
+    protected Disciplina disciplinaSelecionada;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disciplina);
 
         Button button =	(Button) findViewById(R.id.botao);
+
+        this.editTextDisciplin	=	(EditText)	findViewById(R.id.disciplina);
+        this.editTextDisciplin.setText("teste1");
+        Intent intent =	getIntent();
+        disciplinaSelecionada =	(DisciplinaValue)intent.getSerializableExtra("disciplinaSelecionada");
+
+        if(disciplinaSelecionada!=null){
+            button.setText("Alterar");
+            editTextDisciplin.setText(disciplinaSelecionada.getDisciplina());
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DisciplinaDAO dao =	new	DisciplinaDAO(DisciplinaActivity.this);
+                if(disciplinaSelecionada==null)	{
+                    Disciplina disciplina	= new Disciplina();
+                    disciplina.setDisciplina(editTextDisciplin.getText().toString());
+                    dao.salvar(disciplina);
+                }else{
+                    disciplinaSelecionada.setDisciplina(editTextDisciplin.getText().toString());
+                    dao.alterar(disciplinaSelecionada);
+                }
                 finish();
             }
         });
